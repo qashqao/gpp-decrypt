d843d52e2191f88ffa9463f510e5057b689b298c
QashQao Project <33896398+qashqao@users.noreply.github.com>
2018-10-07 16:11:36 +0200

    Update gpp-decrypt.rb

--- gpp-decrypt.rb
+++ gpp-decrypt.rb
@@ -9,16 +9,17 @@ if (a.nil? or a.empty?)
 else
 	def decrypt(a)
 	  	padding = "=" * (4 - (a.length % 4))
-	  	epassword = "#{a}#{padding}"
-	  	decoded = Base64.decode64(epassword)
+	  	passwrd = "#{a}#{padding}"
+	  	decoded = Base64.decode64(passwrd)
 
 	  	key = "\x4e\x99\x06\xe8\xfc\xb6\x6c\xc9\xfa\xf4\x93\x10\x62\x0f\xfe\xe8\xf4\x96\xe8\x06\xcc\x05\x79\x90\x20\x9b\x09\xa4\x33\xb6\x6c\x1b"
 	  	aes = OpenSSL::Cipher.new("AES-256-CBC")
 	  	aes.decrypt
 	  	aes.key = key
-	  	plaintext = aes.update(decoded)
-	  	plaintext << aes.final
-	  	pass = plaintext.unpack('v*').pack('C*') # UNICODE conversion
+	  	
+	  	text = aes.update(decoded)
+	  	text << aes.final
+	  	pass = text.unpack('v*').pack('C*')
 
 		return pass
 	end
