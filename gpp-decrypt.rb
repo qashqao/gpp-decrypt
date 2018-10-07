14bce25de5dd8a1793a9dc5dfb758eeeb053f052
QashQao Project <33896398+qashqao@users.noreply.github.com>
2018-10-07 16:09:59 +0200

    Update gpp-decrypt.rb

--- gpp-decrypt.rb
+++ gpp-decrypt.rb
@@ -2,24 +2,27 @@ require 'rubygems'
 require 'openssl'
 require 'base64'
 
+a = ARGV[0]
+if (a.nil? or a.empty?)
+	print "Encrypted data is not defined\n"
+ 	print "Usage : gpp-decrypt <value-to-decrypt>\n"
+else
+	def decrypt(a)
+	  	padding = "=" * (4 - (a.length % 4))
+	  	epassword = "#{a}#{padding}"
+	  	decoded = Base64.decode64(epassword)
 
-encrypted_data = "j1Uyj3Vx8TY9LtLZil2uAuZkFQA/4latT76ZwgdHdhw"
+	  	key = "\x4e\x99\x06\xe8\xfc\xb6\x6c\xc9\xfa\xf4\x93\x10\x62\x0f\xfe\xe8\xf4\x96\xe8\x06\xcc\x05\x79\x90\x20\x9b\x09\xa4\x33\xb6\x6c\x1b"
+	  	aes = OpenSSL::Cipher.new("AES-256-CBC")
+	  	aes.decrypt
+	  	aes.key = key
+	  	plaintext = aes.update(decoded)
+	  	plaintext << aes.final
+	  	pass = plaintext.unpack('v*').pack('C*') # UNICODE conversion
 
-def decrypt(encrypted_data)
-  padding = "=" * (4 - (encrypted_data.length % 4))
-  epassword = "#{encrypted_data}#{padding}"
-  decoded = Base64.decode64(epassword)
+		return pass
+	end
 
-  key = "\x4e\x99\x06\xe8\xfc\xb6\x6c\xc9\xfa\xf4\x93\x10\x62\x0f\xfe\xe8\xf4\x96\xe8\x06\xcc\x05\x79\x90\x20\x9b\x09\xa4\x33\xb6\x6c\x1b"
-  aes = OpenSSL::Cipher::Cipher.new("AES-256-CBC")
-  aes.decrypt
-  aes.key = key
-  plaintext = aes.update(decoded)
-  plaintext << aes.final
-  pass = plaintext.unpack('v*').pack('C*') # UNICODE conversion
-
-  return pass
+	blah = decrypt(a)
+	puts blah
 end
-
-blah = decrypt(encrypted_data)
-puts blah
